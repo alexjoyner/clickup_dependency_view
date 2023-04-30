f47b09d1e00da0c9e2b3865b156a6a6652c444c1
alexjoyner <rosco9awj@gmail.com>
2023-04-29 20:40:03 -0400

    Basic Setup Of Fetch To ClickupApi

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0)
+	const [API_KEY] = useState(localStorage.get('API_KEY'));
+	const [TARGET_VIEW_ID] = useState(localStorage.get('VIEW_ID'));
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	useEffect(() => {
+		if (!API_KEY) {
+			alert('Please set API_KEY in localStorage for this to work');
+		}
+		if (!TARGET_VIEW_ID) {
+			alert('Please set TARGET_VIEW_ID in localStorage for this to work');
+		}
+	}, [API_KEY, TARGET_VIEW_ID])
+
+	useEffect(() => {
+		const fetchTasks = async () => {
+			console.log('fetching tasks!');
+			const query = new URLSearchParams({
+				page: '0'
+			}).toString();
+
+			const viewId = API_KEY;
+			const resp = await fetch(
+				`https://api.clickup.com/api/v2/view/${viewId}/task?${query}`,
+				{
+					method: 'GET',
+					headers: {
+						Authorization: API_KEY
+					}
+				}
+			);
+
+			const data = await resp.text();
+			console.log(data);
+		}
+
+		fetchTasks();
+	}, [TARGET_VIEW_ID, API_KEY]);
+
+	return (
+		<>
+			<h1>Clickup Dependency View</h1>
+			<div className="card">
+				<button onClick={() => setCount((count) => count + 12)}>
+					count is {count}
+				</button>
+			</div>
+		</>
+	)
 }
 
 export default App
